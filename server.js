b4062626c3c308bfc02c6c94dcb25e5cbd066aed
Tabish <1631995@gmail.com>
2023-07-12 17:01:09 +0200

    good progress

--- server.js
+++ server.js
@@ -36,170 +36,6 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const poemTypes = {
-    1: "limerick",
-    2: "elevenie",
-    "elfje": "elevenie",
-    3: "ollekebolleke",
-    4: "snelsonnet",
-    5: "haiku",
-    6: "romantisch drama plot",
-    7: "tragedie plot",
-    8: "manifest",
-    9: "essay",
-    10: "six word story",
-    11: "toneel opening tragedie",
-    12: "toneel opening komedie",
-    13: "sinterklaasgedicht",
-}
-let messages = [];
-
-function clean(jsonString) {
-    let startIndex = jsonString.indexOf("{");
-    if (startIndex === -1) {
-        return "";
-    }
-    let endIndex = jsonString.lastIndexOf("}");
-    if (endIndex === -1) {
-        return "";
-    }
-    return jsonString.substring(startIndex, endIndex + 1);
-}
-
-function continuePoem(oldWord, newWord) {
-    messages.push({
-        "role": "user",
-        "content": `Can you replace ${oldWord} with ${newWord} in the paragraph text?: Try to make it fit and rewrite the text starting from the replaced word. Change it up a bit, make something cool. Output in Dutch please and the text needs to be the same poem type as the original text.`
-    });
-
-    messages.push({
-        "role": "system",
-        "content": "Output format in a valid JSON like {paragraph: '...', keywords: [{keyword: lopen, alternatives: [rennen, springen, ...]}, ...]}." +
-            "Also use proper punctuation, no weird characters like newlines. Extract between 2 and 6 keywords from the text, " +
-            "which are of importance for the text. Can you generate some other words for each keyword. these keywords need to be completely unrelated. For example voetbal: honkbal, schilderen, koken. or lopen: schilderen, zwemmen, zeuren, praten or mooi: lang, kort, lelijk, goed." +
-            "I only want the Output in a valid JSON format like {paragraph: '...', keywords: [{keyword: lopen, alternatives: [rennen, springen, ...]}, ...]}."
-    });
-
-    return messages;
-}
-
-function api_generate(poemType, themes) {
-    let msgs = [];
-    poemType = poemTypes[poemType] || poemType;
-    msgs.push({
-        "role": "user",
-        "content": `Can you create part of a Dutch ${poemType} about these themes?: ${themes}`
-    });
-
-
-    switch (poemType) {
-        case "limerick":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een limerick van vijf regels met een metrum van twee regels met drie amfibrachen, " +
-                    "twee regels met een amfibrachys en een jambe en afgesloten door weer een regel met drie amfibrachen met een rijmschema is a a b b a."
-            });
-            break;
-        case "elevenie":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een elfje van elf woorden op vijf dichtregels. " +
-                    "De woorden worden als volgt verdeeld de eerste dichtregel: een woord de tweede regel: twee woorden de derde regel: " +
-                    "drie woorden de vierde regel: vier woorden de vijfde regel: één woord, dat het gedicht samenvat. "
-            });
-            break;
-        case "ollekebolleke":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een Ollekebolleke met twee strofen van elk vier versregels met schema abcd efgd gebruik metrum dactylus met varianten."
-            });
-            break;
-        case "snelsonnet":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een snelsonnet met een kwatrijn van 4 regels, gevolgd door een distichon van 2 regels. " +
-                    "Als metrum dient de vijfvoetige jambe met 10 of 11 lettergrepen, afwisselend onbeklemtoond en beklemtoond. " +
-                    "Het rijm van het kwatrijn heeft rijmschema A-B-B-A en de regels van het distichon rijmen op elkaar met rijmschema: C-C " +
-                    "maar nooit op een van beide reeds in het kwatrijn gebruikte rijmklanken. Na de laatste zin van het kwatrijn volgt een chute, " +
-                    "zodanig dat het distichon de in het kwatrijn betrokken stelling relativeert."
-            });
-            break;
-        case "haiku":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een Haiku van drie regels waarvan de eerste regel 5, de tweede regel 7 en de derde regel weer 5 lettergrepen telt."
-            });
-            break;
-
-        case "essay":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een essay van max 500 woorden over het onderwerp."
-            });
-            break;
-        case "six word story":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een six word story van maximaal 6 woorden."
-            });
-            break;
-        case "toneel opening tragedie":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een toneel openings dialoog van een tragedie voor de eerste akte. So this is a script dialogue between one or more characters, I want this to be like for example bobby: blablabla, susan: blablabla."
-            });
-            break;
-        case "toneel opening komedie":
-            msgs.push({
-                "role": "system",
-                "content": "Schrijf een toneel openings dialoog van een komedie voor de eerste akte. So this is a script dialogue between one or more characters, I want this to be like for example bobby: blablabla, susan: blablabla."
-            });
-            break;
-
-        case "sinterklaasgedicht":
-            msgs.push({
-                "role": "system",
-                "content": "Write a Dutch saint nicholas poem. This poem has to tease the recipient a bit. Rhyme scheme is aa bb cc. It is mandatory for this to rhyme IN DUTCH based on the last word of the sentence!" +
-                    "Exactly 2 lines please. A line has to end with a period. I want a newline character for each line."
-            });
-            break;
-    }
-
-    msgs.push({
-        "role": "system",
-        "content": "Output format in a valid JSON like {\"paragraph\": \"this is a paragraph\"}. This JSON has to be parsable! Also use proper punctuation."
-    });
-
-
-    return msgs;
-}
-
-function api_alt_gen(keywords) {
-    let msgs = [];
-    msgs.push({
-        "role": "user",
-        "content": `Can you generate some other words for each keyword these keywords need to be completely unrelated. 
-        For example voetbal: honkbal, schilderen, koken. or lopen: schilderen, zwemmen, zeuren, praten or mooi: lang, kort, lelijk, goed. Here are the keywords: ${keywords}`
-    });
-
-    msgs.push({
-        "role": "system",
-        "content": "Between 3 and 10 alternatives for each keyword. The output format in a valid JSON like {keywords: [{keyword: lopen, alternatives: [bakken, huilen, ...]}]}"
-    });
-
-    return msgs;
-}
-
-async function getCompletion(prompts, isTurbo) {
-    const completion = await openai.createChatCompletion({
-        model: isTurbo ? "gpt-3.5-turbo" : "gpt-4",
-        messages: prompts,
-    });
-
-    let output = clean(completion.data.choices[0].message.content);
-    return JSON.parse(output);
-}
-
 const start = async () => {
     const keys = new Set([process.env.BK]);
     await fastify
@@ -228,6 +64,53 @@ const start = async () => {
             });
     });
 
+    fastify.post("/initarg", async (request, reply) => {
+        let cookie = request.cookies.__sesh;
+        if (!cookie || !fastify.unsignCookie(cookie).valid) {
+            reply.status(401).send({"message": "Unauthorized"});
+        } else {
+
+
+            let statement = request.body.statement
+            console.log(statement)
+
+            let msgs = [];
+            msgs.push({
+                "role": "user",
+                "content": `De stelling is : ${statement} Verzin vijf argumenten die kloppen met de stelling. Hou ze kort en in het nederlands! Max 15 woorden per argument. Begin met een kleine letter.
+            Kun je in de prompt voor de argumenten verwerken dat hij de argumenten moet baseren op deze onderwijswaarden?: Onderwijswaarden: rechtvaardigheid (gelijke kansen, inclusiviteit, integriteit), 
+            menselijkheid (sociale samenhang, respect, veiligheid, welzijn), 
+            autonomie (zelfbeschikking, privacy, onafhankelijkheid, vrijheid).
+            Maak de argumenten grammaticaal correct en passend in de context van de statement wanneer deze wordt geplaatst. Dus de argumenten moet passen op de plek
+            van de ...
+            Do not include any explanations, only provide a RFC8259 compliant JSON response  following this format without deviation.
+                        Like {arguments: [arg1, arg2]}.`
+            });
+
+            const completion = await openai.createChatCompletion({
+                model: "gpt-4",
+                messages: msgs,
+            });
+
+            let data = JSON.parse(completion.data.choices[0].message.content)
+
+            return `<div id="statement-text">
+                    ${statement.split("…")[0]}
+                    <select id="argument">
+                    ${data.arguments.map((arg) => {
+                return `<option value="${arg}">${arg}</option>`
+            })}
+                    </select>
+                    <input type="hidden" id="statement-input" name="statement" value=\`\`>   
+                    </div>
+                    <script>
+                        document.getElementById("argument").addEventListener("change", () => {
+                        document.getElementById("statement-input").value = (document.getElementById("statement-text").innerHTML.split("<")[0] + document.getElementById("argument").value); 
+                        });
+                    </script>`
+        }
+    });
+
 
     fastify.post('/consensus', async (request, reply) => {
         let cookie = request.cookies.__sesh;
@@ -249,9 +132,29 @@ const start = async () => {
                         Like {eens: ["eens argument"], oneens: ["oneens argument"], mits: ["mits argument"]}.`
             });
 
+            let msgs2 = [];
+            msgs2.push({
+                "role": "user",
+                "content": `${statement} \nSchrijf drie versies van deze statement die eens, oneens, of mits zijn met de originele statement. " +
+                    "Probeer zo min mogelijk van de originele statement aan te passen, max drie woorden. " +
+                    "Geef bij elk van deze statements een woordgroep met aaneengesloten woorden die per zin van elkaar verschillen " +
+                    "en die het duidelijkst aangeven wat de opinie van de statement is. Geef alleen de zin"
+                     Do not include any explanations, only provide a RFC8259 compliant JSON response following this format without deviation.
+                     Like {eens: {statement: "eens statement", replacement: "words to be replaced"}, 
+                     oneens: {statement: "oneens statement", replacement: "words to be replaced"}, 
+                     mits: {statement: "mits argument", replacement: "words to be replaced"}}.\``
+            });
+
             const completion = await openai.createChatCompletion({
                 model: "gpt-4",
                 messages: msgs,
+            });
+
+            const completion2 = await openai.createChatCompletion({
+                model: "gpt-4",
+                messages: msgs2,
+            }).then((res) => {
+                console.log(res.data.choices[0].message.content)
             });
 
             let args = JSON.parse(completion.data.choices[0].message.content)
@@ -259,7 +162,7 @@ const start = async () => {
                 <optgroup label="eens">
                 ${args.eens.map((arg) => `<option>${arg}</option>`)}
                 </optgroup>
-                <optgroup label="oneens">  
+                <optgroup label="oneens">
                 ${args.oneens.map((arg) => `<option>${arg}</option>`)}
                 </optgroup>
                 <optgroup label="mits">
